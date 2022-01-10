4a98fa9778452afa1a82d53ab708cdf7ecc580c3
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-01-10 15:35:09 -0600

    Setup backend/frontend for login and signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
-import {useNavigate} from "react-router-dom"
-
+import { useNavigate } from 'react-router';
 function SignupForm({setUser}) {
 
     let navigate = useNavigate();
