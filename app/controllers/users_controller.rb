bd6dbf4a140986354f0b0e27bba94e4a25951675
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-01-13 20:15:27 -0600

    can see champions/abilities, continue debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
     def create
         user = User.new(user_params)
+        byebug
         if user.save
+            byebug
             session[:user_id] = user.id
             byebug
             render json: user, status: :created
